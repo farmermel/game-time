9a21b2618b17208bf3b1f912f7af70f5d1bd1ecf
Melena Suliteanu <melenasuliteanu@gmail.com>
2017-12-05 20:01:01 -0700

    Test ufo and refuel
    put in note for what to test on game

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,14 +1,40 @@
-// const { assert } = require('chai');
-// const Game = require('../lib/Game.js');
+const { assert } = require('chai');
+const Game = require('../lib/Game.js');
 
-// describe('Game', function() {
-//   let game;
+describe('Game', function() {
+  let game;
 
-//   beforeEach(function(){
-//     game = new Game();
-//   });
+  beforeEach(function(){
+    game = new Game();
+  });
 
-//   it('should load start screen first', function() {
-//    
-//   })
-// })
+  //movePlanets
+  //testCollision
+  //changelevelstate
+  //generate refuel
+
+  //on loss:
+  //reset states
+  //reset score
+  //reset level
+
+  //on levelup
+  //change level
+  //put back bottom
+  //reset planet array
+  //reset states in general
+
+  //test calculate score
+
+  //check that refuel exists
+  //check that planets are being removed
+
+
+  it('should ')
+
+  it('should remove a planet from planet array on collision', function() {
+
+  })
+
+
+})
